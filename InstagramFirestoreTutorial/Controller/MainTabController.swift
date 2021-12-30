6887fabadf008bedac53d02074397a6f674ae63a
justinlove0987 <justinlove0987@gmail.com>
2021-12-30 21:56:05 +0800

    03 finish tab bar setup

--- InstagramFirestoreTutorial/Controller/MainTabController.swift
+++ InstagramFirestoreTutorial/Controller/MainTabController.swift
@@ -8,7 +8,6 @@
 import UIKit
 
 class MainTabController: UITabBarController {
-    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
@@ -20,17 +19,36 @@ class MainTabController: UITabBarController {
     
     func configureViewController() {
         view.backgroundColor = .white
-        let feed = FeedController()
+        let feed = templateNavigationController(unselectedImage: UIImage(named: "home_unselected")!,
+                                                selectedImage: UIImage(named: "home_selected")!,
+                                                rootViewController: FeedController())
         
-        let search = SearchController()
+        let search = templateNavigationController(unselectedImage: UIImage(named: "search_unselected")!,
+                                                  selectedImage: UIImage(named: "search_selected")!,
+                                                  rootViewController: FeedController())
         
-        let imageSelector = ImageSelectorController()
+        let imageSelector = templateNavigationController(unselectedImage: UIImage(named: "plus_unselected")!,
+                                                         selectedImage: UIImage(named: "plus_unselected")!,
+                                                         rootViewController: ImageSelectorController())
         
-        let notification = NotificationController()
+        let notification = templateNavigationController(unselectedImage: UIImage(named: "like_unselected")!,
+                                                        selectedImage: UIImage(named: "like_selected")!,
+                                                        rootViewController: NotificationController())
         
-        let profile = ProfileController()
+        let profile = templateNavigationController(unselectedImage: UIImage(named: "profile_unselected")!,
+                                                   selectedImage: UIImage(named: "profile_selected")!,
+                                                   rootViewController: NotificationController())
         
         viewControllers = [feed, search, imageSelector, notification, profile]
+        tabBar.tintColor = .black
+    }
+    
+    func templateNavigationController(unselectedImage: UIImage, selectedImage: UIImage, rootViewController: UIViewController) -> UINavigationController {
+        let nav = UINavigationController(rootViewController: rootViewController)
+        nav.tabBarItem.image = unselectedImage
+        nav.tabBarItem.selectedImage = selectedImage
+        nav.navigationBar.tintColor = .black
+        return nav
     }
     
 }
