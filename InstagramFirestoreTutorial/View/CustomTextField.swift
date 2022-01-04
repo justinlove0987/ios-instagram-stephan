1b870856457cd4587fdf17cc06ee66b48e440b9d
justinlove0987 <justinlove0987@gmail.com>
2022-01-04 21:52:06 +0800

    08 creating custom subclasses for reusability

---
+++ InstagramFirestoreTutorial/View/CustomTextField.swift
@@ -0,0 +1,35 @@
+//
+//  CustomTextField.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 曾柏瑒 on 2022/1/4.
+//
+
+import UIKit
+
+class CustomTextField: UITextField {
+    
+    init(placeholder: String) {
+        super.init(frame: .zero)
+        
+        let spacer = UIView()
+        spacer.setDimensions(height: 50, width: 12)
+        leftView = spacer
+        leftViewMode = .always
+        
+        borderStyle = .none
+        textColor = .white
+        keyboardAppearance = .dark
+        keyboardType = .emailAddress
+        backgroundColor = UIColor(white: 1, alpha: 0.1)
+        setHeight(50)
+        attributedPlaceholder = NSAttributedString(string: placeholder,
+                                                   attributes: [.foregroundColor: UIColor(white: 1, alpha: 0.7)])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+}
