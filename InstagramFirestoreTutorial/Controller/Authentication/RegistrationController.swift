b8a1cdaf55dc969dbcfbf69f96329e51a6210ba2
justinlove0987 <justinlove0987@gmail.com>
2022-01-04 15:20:44 +0800

    07 login page ui

---
+++ InstagramFirestoreTutorial/Controller/Authentication/RegistrationController.swift
@@ -0,0 +1,24 @@
+//
+//  RegistrationController.swift
+//  InstagramFirestoreTutorial
+//
+//  Created by 曾柏瑒 on 2022/1/4.
+//
+
+import UIKit
+
+class RegistrationController: UIViewController {
+    
+    // MARK: - Properties
+    
+    // MARK: - Lifecycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .systemPink
+    }
+    
+    
+}
+
