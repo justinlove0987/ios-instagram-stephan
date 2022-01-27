0b749b4bd90bc70c0725ea3c042a815d09c366bb
justinlove0987 <justinlove0987@gmail.com>
2022-01-27 15:24:23 +0800

    23 fetching & displaying users

--- InstagramFirestoreTutorial/API/UserService.swift
+++ InstagramFirestoreTutorial/API/UserService.swift
@@ -15,7 +15,20 @@ struct UserService {
             // .data return dictionary back
             guard let dictionary = snapshot?.data() else { return }
             let user = User(dictionary: dictionary)
+            
             completion(user)
         }
+    }
+    
+    static func fetchUsers(completion: @escaping([User])  -> Void) {
+        COLLECTION_USERS.getDocuments { snapshot, error in
+            guard let snapshot = snapshot else { return }
+            
+            let users = snapshot.documents.map({ User(dictionary: $0.data() ) })
+            completion(users)
+            
+                    
+        }
+        
     }
 }
--- InstagramFirestoreTutorial/Controller/SearchController.swift
+++ InstagramFirestoreTutorial/Controller/SearchController.swift
@@ -13,12 +13,25 @@ class SearchController: UITableViewController {
     
     // MARK: - Properties
     
+    private var users = [User]()
+    
     // MARK: - Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureTableView()
+        fetchUsers()
+    }
+    
+    // MARK: - API
+    
+    func fetchUsers() {
+        UserService.fetchUsers { users in
+            print("DEBUG: \(users)")
+            self.users = users
+            self.tableView.reloadData()
+        }
     }
     
     // MARK: - Helpers
@@ -35,11 +48,12 @@ class SearchController: UITableViewController {
 
 extension SearchController {
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return users.count
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: reuseIdentifier, for: indexPath) as! UserCell
+        cell.user = users[indexPath.row]
         return cell
     }
     
