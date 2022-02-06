ee2c4b73706b9a4f4db922b498744f0d04c046f8
justinlove0987 <justinlove0987@gmail.com>
2022-02-06 17:02:06 +0800

    28 user stats

--- InstagramFirestoreTutorial/API/UserService.swift
+++ InstagramFirestoreTutorial/API/UserService.swift
@@ -57,9 +57,21 @@ struct UserService {
             guard let isFollowed = snapshot?.exists else { return }
             completion(isFollowed)
         }
-        
-        
     }
+    
+    static func fetchUserStats(uid: String, completion: @escaping(UserStats) -> Void) {
+        COLLECTION_FOLLOWERS.document(uid).collection("user-followers").getDocuments { snapshot, _ in
+            let followers = snapshot?.documents.count ?? 0
+            
+            COLLECTION_FOLLOWING.document(uid).collection("user-following").getDocuments { snapshot, _ in
+                let following = snapshot?.documents.count ?? 0
+                
+                completion(UserStats(followers: followers, following: following))
+            }
+        }
+    }
+    
+    
     
     
 }
